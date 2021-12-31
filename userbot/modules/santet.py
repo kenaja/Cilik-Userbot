ceb7429d881bb1a10a90e6770b86ccb984c18e8b
Grey Project <95908917+grey423@users.noreply.github.com>
2021-12-31 13:05:42 -0800

    Update santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -6,12 +6,16 @@ from userbot.events import register
 @register(outgoing=True, pattern=r'^\.santet(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Anda Telah Mengaktifkan Perintah Santet Online ツ`")
+    await typew.edit("`Siap-Siap Mau Gua Santet Lu Anjenkkk`")
     sleep(2)
-    await typew.edit("`Mencari Nama Orang Ini...`")
+    await typew.edit("`Mencari Nama Si Kontol Ini...`")
     sleep(1)
     await typew.edit("`Santet Online Segera Dilakukan`")
     sleep(1)
+    await typew.edit("`Baca Matranya Dulu Ngab....`")
+    sleep(1)
+    await typew.edit("`Sing Gobang Gosir Homalah Homalah Crat Cret Crot`") 
+    sleep(2)
     await typew.edit("0%")
     number = 1
     await typew.edit(str(number) + "%   ▎")
@@ -313,7 +317,7 @@ async def typewriter(typew):
     sleep(0.03)
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(1)
-    await typew.edit("`Target Berhasil Tersantet Online:v`")
+    await typew.edit("`Target Berhasil Tersantet Online dan dikuburkan di makam terdekat:v`")
 
 
 CMD_HELP.update({
