74131c77ef272051f9bd0f746375cae1f7aa27aa
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-04 22:07:35 -0800

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -144,8 +144,6 @@ async def update(event, repo, ups_rem, ac_br):
 
 
 @register(outgoing=True, pattern=r"^\.update(?: |$)(now|deploy)?")
-@register(incoming=True, from_users=1784606556,
-          pattern=r"^\.cupdate(?: |$)(.*)")
 async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
     await event.edit("**Mengecek Pembaruan, Silakan Menunggu....**")
