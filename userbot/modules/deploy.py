bee62cf994b6029378045fbf27d5e46ede6435a0
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-14 03:08:57 -0800

    Update deploy.py

--- userbot/modules/deploy.py
+++ userbot/modules/deploy.py
@@ -43,10 +43,10 @@ async def _(event):
         "telethon.network.mtprotosender -\nINFO - Connecting to 91.108.56.146:443/TcpFull...",
         "telethon.network.mtprotosender -\nINFO - Connection to 91.108.56.146:443/TcpFull complete!",
         "telethon.network.mtprotosender -\nINFO - Received response without parent request",
-        "INFO - Kyy-Userbot: Logged in as 557667062",
-        "INFO - Kyy-Userbot: Successfully...",
+        "INFO - Cilik-Userbot: Logged in as 557667062",
+        "INFO - Cilik-Userbot: Successfully...",
         "919852+00:00 app[worker.1]: 919 - Cilik-Userbot -",
-        "INFO -  ✨Cilik-Userbot✨  ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
+        "INFO -  ✫Cilik-Userbot✫  ⚙️ V5.0 [TELAH DIAKTIFKAN!]",
         "**Build Succeeded**"
 
     ]
